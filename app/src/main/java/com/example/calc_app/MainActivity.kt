c45126e1e2f8594d53f3bd4bcef0b16a96b56c05
Matheus Freitas <mgsoutodelira@gmail.com>
2025-09-29 10:48:03 -0300

    up

--- app/src/main/java/com/example/calc_app/MainActivity.kt
+++ app/src/main/java/com/example/calc_app/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calculadora)
 
         tvDisplay = findViewById(R.id.txtResultado)
         tvHistory = findViewById(R.id.txtHistorico)
